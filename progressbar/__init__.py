506c92f8b84a01799f06cb45cb8831ef85a0639c
Nilton Volpato <nilton.volpato@gmail.com>
2018-06-28 19:31:41 -0700

    use a real email since setuptools complain about it now

--- progressbar/__init__.py
+++ progressbar/__init__.py
@@ -40,7 +40,7 @@ automatically enable features like auto-resizing when the system supports it.
 """
 
 __author__ = 'Nilton Volpato'
-__author_email__ = 'first-name dot last-name @ gmail.com'
+__author_email__ = 'nilton.volpato@gmail.com'
 __date__ = '2011-05-14'
 __version__ = '2.5'
 
