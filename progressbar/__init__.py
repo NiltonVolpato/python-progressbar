93769a33b55a75a821029d47fd7ce213dddfb07b
Nilton Volpato <nilton@volpa.to>
2026-03-10 04:58:52 -0700

    new version
    Signed-off-by: Nilton Volpato <nilton@volpa.to>

--- progressbar/__init__.py
+++ progressbar/__init__.py
@@ -39,10 +39,10 @@ The progressbar module is very easy to use, yet very powerful. It will also
 automatically enable features like auto-resizing when the system supports it.
 """
 
-__author__ = 'Nilton Volpato'
-__author_email__ = 'nilton.volpato@gmail.com'
-__date__ = '2011-05-14'
-__version__ = '2.5'
+__author__ = "Nilton Volpato"
+__author_email__ = "nilton.volpato@gmail.com"
+__date__ = "2026-03-09"
+__version__ = "2.6"
 
 from .compat import *
 from .widgets import *
